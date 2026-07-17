728b469f4929904edf1c603712f0cdf62dda0acd
NxHup <nicelyshop101@gmail.com>
2026-07-17 23:58:45 +0700

    Update NxHub.lua

--- NxHub.lua
+++ NxHub.lua
@@ -3,23 +3,24 @@
 --   By_Beemyeon
 -- ============================================================
 
-local MAIN_WORLD_ID = 119091355492870
-local currentPlaceId = game.PlaceId
+local MAIN_WORLD_ID    = 119091355492870
+local DUNGEON_WORLD_ID = 82878101790702
+local currentPlaceId   = game.PlaceId
 
 print("[Nx Hub Router] Current PlaceId:", currentPlaceId)
 
-if currentPlaceId == MAIN_WORLD_ID then
-    print("[Nx Hub Router] Loading Main World Mode...")
-    if isfile and isfile("NxHub_Main.lua") then
-        loadfile("NxHub_Main.lua")()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NxHup/Nx-Hub/main/NxHub_Main.lua"))()
-    end
-else
+if currentPlaceId == DUNGEON_WORLD_ID then
     print("[Nx Hub Router] Loading Dungeon Mode...")
     if isfile and isfile("NxHub_Dungeon.lua") then
         loadfile("NxHub_Dungeon.lua")()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/NxHup/Nx-Hub/main/NxHub_Dungeon.lua"))()
+    end
+else
+    print("[Nx Hub Router] Loading Main World Mode...")
+    if isfile and isfile("NxHub_Main.lua") then
+        loadfile("NxHub_Main.lua")()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NxHup/Nx-Hub/main/NxHub_Main.lua"))()
     end
 end
