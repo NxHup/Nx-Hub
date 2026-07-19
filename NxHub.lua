adab82501a6a33ca9f4d8ef02bdddefac72cc489
NxHup <nicelyshop101@gmail.com>
2026-07-19 23:15:34 +0700

    Update NxHub.lua

--- NxHub.lua
+++ NxHub.lua
@@ -2,7 +2,7 @@
 local NxHubRouter = {
     ["Rock Fruit Main"] = {
         ID = 119091355492870,
-        -- เอา ?v=os.time() ออก เพื่อป้องกัน Error 429
+      
         HTTP = "https://raw.githubusercontent.com/NxHup/Nx-Hub/refs/heads/main/NxHub_Main.lua"
     },
     ["Rock Fruit Dungeon"] = {
@@ -14,26 +14,26 @@ local NxHubRouter = {
 local currentPlaceId = game.PlaceId
 local isSupported = false
 
--- 2. วนลูปเช็กแมพที่รองรับ
+
 for _, mapConfig in pairs(NxHubRouter) do
     if currentPlaceId == mapConfig.ID then
         isSupported = true
         print("[Nx Hub] Matching map found! Preparing to load...")
         
-        -- 3. ใช้ pcall ครอบไว้ ป้องกันสคริปต์เด้งสีแดงเวลา GitHub หรือเน็ตผู้เล่นมีปัญหา
+
         local success, err = pcall(function()
             loadstring(game:HttpGet(mapConfig.HTTP))()
         end)
         
         if not success then
             warn("[Nx Hub Error] ไม่สามารถโหลดสคริปต์หลักได้: " .. tostring(err))
-            -- ตรงนี้สามารถใส่ระบบแจ้งเตือน (Notification) ในเกมให้ผู้เล่นรู้ได้ว่าเซิร์ฟเวอร์ล่ม
+
         end
         break
     end
 end
 
--- 4. ถ้าไม่เจอแมพที่รองรับ ให้เตะออกทันทีเพื่อความปลอดภัย
+
 if not isSupported then
     local Players = game:GetService("Players")
     if Players.LocalPlayer then
